64cb8d6c72b9bc30c1da31d97b689cb5f099dd17
Xiaobonor <xiao.bo.nor@gmail.com>
2024-10-08 00:46:47 +0800

    redis client in workflow bug fix

--- app/utils/redis_client.py
+++ app/utils/redis_client.py
@@ -2,4 +2,13 @@
 from redis import Redis
 import os
 
-redis_client = Redis.from_url(os.getenv('REDIS_URI'))
+redis_uri = os.getenv('REDIS_URI')
+redis_client = None
+
+if redis_uri:
+    try:
+        redis_client = Redis.from_url(redis_uri)
+    except Exception as e:
+        print(f"Error initializing Redis: {e}")
+else:
+    print("REDIS_URI not set, skipping Redis initialization.")
