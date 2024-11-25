111a1038bb8a1d8b0a71c2663f7b149913640285
nonepork <59335048+nonepork@users.noreply.github.com>
2024-11-25 22:45:26 +0800

    a

--- app/routes/application.py
+++ app/routes/application.py
@@ -143,7 +143,7 @@ def first_part():
 
         # Generate redirect url
 
-        accept_url = f"{os.getenv("NEXT_FORM_URL")}?{str(secret)}"
+        accept_url = f"{os.getenv("NEXT_FORM_URL")}?{secret}"
         print(accept_url)
 
         # Saves to database
