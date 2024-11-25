155250fc9d3d6738020d58160a7ce742abe79889
nonepork <59335048+nonepork@users.noreply.github.com>
2024-11-25 23:26:51 +0800

    Finish second part

--- app/routes/application.py
+++ app/routes/application.py
@@ -234,7 +234,7 @@ def second_part():
 @application_bp.route("/testing", methods=["POST"])
 def testing():
     try:
-        form_data = request.json.get("answers")
+        form_data = request.json.get("hiddenFields")
 
         print(form_data)
 
