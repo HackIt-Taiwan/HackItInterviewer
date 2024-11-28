60f1fc0c24180c923c8cb1b1cb08bf8df69ef834
nonepork <59335048+nonepork@users.noreply.github.com>
2024-11-28 22:44:28 +0800

    Testing

--- app/discord/application_process/__init__.py
+++ app/discord/application_process/__init__.py
@@ -1,5 +1,6 @@
 # app/discord/application_process/__init__.py
-# def setup(bot):
+def setup(bot):
+    pass
 #     """Initialize the application_process module."""
 #     from .commands import setup as commands_setup
 #
