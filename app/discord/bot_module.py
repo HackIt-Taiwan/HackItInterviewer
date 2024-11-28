f4426a5fc4bf42b86e8edfc7aabf92556d5abf8f
nonepork <59335048+nonepork@users.noreply.github.com>
2024-11-28 22:40:34 +0800

    Testing

--- app/discord/bot_module.py
+++ app/discord/bot_module.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from app.discord.application_process import setup as application_process_setup
-from app.utils.mail_sender import send_email
 
 intents = discord.Intents.default()
 intents.message_content = True
