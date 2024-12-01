3d9acb7543afb1bd02aca8fb32a2c69146cd1852
Yorukot <ben.hrc57@gmail.com>
2024-12-01 22:18:57 +0800

    update

--- app/routes/application.py
+++ app/routes/application.py
@@ -154,10 +154,7 @@ def first_part():
         # Sends to discord
 
     
-        future = asyncio.run_coroutine_threadsafe(
-            send_initial_embed(form_response), bot.loop
-        )
-        future.result()
+        bot.loop.create_task(send_initial_embed(form_response))
 
         return jsonify({"status": "ok"}), 200
     except Exception as e:
