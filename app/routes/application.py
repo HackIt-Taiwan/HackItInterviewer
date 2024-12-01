3d4fef5f80e914eec85b1f2fb0bb20ce8f0b562d
Yorukot <ben.hrc57@gmail.com>
2024-12-01 21:45:25 +0800

    feat: updata back

--- app/routes/application.py
+++ app/routes/application.py
@@ -151,7 +151,10 @@ def first_part():
 
         # Sends to discord
 
-        result = send_initial_embed(form_response)
+        future = asyncio.run_coroutine_threadsafe(
+            send_initial_embed(form_response), bot.loop
+        )
+        future.result()
 
         return jsonify({"status": "ok"}), 200
     except Exception as e:
