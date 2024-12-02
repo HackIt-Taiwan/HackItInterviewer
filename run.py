8bb99b35a686bac091f9ed3923c8e167c899e7aa
nonepork <59335048+nonepork@users.noreply.github.com>
2024-12-02 20:58:03 +0800

    BREAKING CHANGE: swapped to gunicore and gevent

--- run.py
+++ run.py
@@ -1,20 +1,38 @@
 # app.py
-import threading
-from app.discord.bot_module import bot
-from app import create_app
+from gevent import monkey
+
+monkey.patch_all()  # This allows Gevent to monkey-patch standard libraries for non-blocking IO
+
+import gevent
 import os
+from application.discord.bot_module import bot
+from app import create_app
 
 app = create_app()
 
 
-def run_api():
+def start_flask():
     host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", 80))
-    app.run(debug=app.config["DEBUG"], host=host, port=port)
+    app.run(
+        debug=app.config["DEBUG"],
+        host=host,
+        port=port,
+        threaded=True,
+        use_reloader=False,
+    )
+
+
+def start_discord_bot():
+    bot.run(os.getenv("DISCORD_TOKEN"))
 
 
 if __name__ == "__main__":
-    web_thread = threading.Thread(target=run_api)
-    web_thread.start()
+    # Start Flask in a gevent greenlet
+    flask_greenlet = gevent.spawn(start_flask)
 
-    bot.run(os.getenv("DISCORD_TOKEN"))
+    # Start the Discord bot in a separate greenlet
+    discord_greenlet = gevent.spawn(start_discord_bot)
+
+    # Wait for both tasks to finish
+    gevent.joinall([flask_greenlet, discord_greenlet])
