0ceae6f978f5f21aaaec0a5a22380667e1bdab23
nonepork <59335048+nonepork@users.noreply.github.com>
2024-12-02 21:13:20 +0800

    fix: restored

--- run.py
+++ run.py
@@ -1,38 +1,20 @@
 # app.py
-from gevent import monkey
-
-monkey.patch_all()  # This allows Gevent to monkey-patch standard libraries for non-blocking IO
-
-import gevent
-import os
+import threading
 from app.discord.bot_module import bot
 from app import create_app
+import os
 
 app = create_app()
 
 
-def start_flask():
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 80))
-    app.run(
-        debug=app.config["DEBUG"],
-        host=host,
-        port=port,
-        threaded=True,
-        use_reloader=False,
-    )
-
-
-def start_discord_bot():
-    bot.run(os.getenv("DISCORD_TOKEN"))
+def run_discord_bot():
+    bot.run(os.getenv('DISCORD_TOKEN'))
 
 
 if __name__ == "__main__":
-    # Start Flask in a gevent greenlet
-    flask_greenlet = gevent.spawn(start_flask)
+    bot_thread = threading.Thread(target=run_discord_bot)
+    bot_thread.start()
 
-    # Start the Discord bot in a separate greenlet
-    discord_greenlet = gevent.spawn(start_discord_bot)
-
-    # Wait for both tasks to finish
-    gevent.joinall([flask_greenlet, discord_greenlet])
+    host = os.getenv('HOST', '0.0.0.0')
+    port = int(os.getenv('PORT', 80))
+    app.run(debug=app.config['DEBUG'], host=host, port=port)
