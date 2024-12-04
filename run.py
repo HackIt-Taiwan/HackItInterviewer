7579f3e82d846a51d0b119e241090505150e31cc
Yorukot <ben.hrc57@gmail.com>
2024-12-05 00:41:35 +0800

    fix: fix bot not running

--- run.py
+++ run.py
@@ -13,10 +13,9 @@ def run_discord_bot():
 
 # Bad idea, should've used quart or something much more elegant.
 if __name__ == "__main__":
-    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-        # Run the bot only in the reloader process
-        bot_thread = threading.Thread(target=run_discord_bot)
-        bot_thread.start()
+    # Run the bot only in the reloader process
+    bot_thread = threading.Thread(target=run_discord_bot)
+    bot_thread.start()
 
     host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", 80))
